8df8960fee3a1e8e670cc70cfcd29519007fe2f3
LucasMelvin15 <melvinlucas15@gmail.com>
2022-08-10 17:49:38 +0300

    complete project

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const{ensureAuth, ensureGuest} = require('../middleware/auth')
+const Story= require('../models/Story')
 
 // @dsk Login/Landing page
 //@route GET/
@@ -11,8 +12,18 @@ router.get('/', ensureGuest, (req,res)=>{
 })
 // @dsk Dashboard
 //@route GET/
-router.get('/dashboard', ensureAuth, (req,res)=>{
-    console.log(req.user)
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req,res)=>{
+    try{
+        const stories = await Story.find({user: req.user.id}).lean()
+      res.render('dashboard',{
+        name:req.user.firstName,
+        stories
+    })
+    }catch(err){
+        console.error(err)
+        res.render('error/500')
+    }
+    
+    
 })
 module.exports = router
